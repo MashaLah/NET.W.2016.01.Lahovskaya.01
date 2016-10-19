1880e940e4c2fb9e99bd5d2ccc8c767d4f669fbf
MashaLah <lahoska91@mail.ru>
2016-10-20 02:19:06 +0300

    Task1

---
+++ Task1/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class BinarySearch
+    {
+        public static int Search(int x, int[] array)
+        {
+            if (array != null)
+            {
+                int low = 0, high = array.Length;
+                while (low < high)
+                {
+                    int mid = low + (high - low) / 2;
+                    if (x == array[mid])
+                    {
+                        return mid;
+                    }
+                    else if (x < array[mid])
+                    {
+                        high = mid;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ task1.UI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task1UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 8, 12, 13, 45, 64, 77, 78, 88, 325 };
+            foreach (int a in array) Console.Write("{0} ", a);
+            Console.WriteLine();
+            Console.Write("Enter x: ");
+            int x = Convert.ToInt16(Console.ReadLine());
+            int result = BinarySearch.Search(x, array);
+            if (result >= 0)
+            {
+                Console.WriteLine("Index = {0}", result);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+            Console.ReadLine();
+        }
+    }
+}
