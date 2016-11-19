722a42692608636070a7c0f89ae26b16dfc14d5b
MashaLah <lahoska91@mail.ru>
2016-11-19 23:55:04 +0300

    remove

--- task1.UI/Program.cs
+++ task1.UI/Program.cs
@@ -15,7 +15,7 @@ namespace Task1UI
             foreach (int a in array) Console.Write("{0} ", a);
             Console.WriteLine();
             Console.Write("Enter x: ");
-            int x = Int32.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
             int result = BinarySearch.Search(x, array);
             if (result >= 0)
             {
